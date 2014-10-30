bd55c8c460c6f27aeb11abfb4c6a3daf3716dcbc
Josh Milo <josh@joshmilo.com>
2014-10-30 14:14:57 -0700

    updating bower.json, added routes and data

--- bubble-stocks.py
+++ bubble-stocks.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+from stock_scraper import get_data
+import os
+
 
 app = Flask(__name__)
 
 
+@app.route('/data')
+def data():
+    return jsonify(get_data())
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def data():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
