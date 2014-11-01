76f28304f86ae99bfa649263cd5aeb95e2fec840
Josh Milo <josh@joshmilo.com>
2014-10-31 19:57:49 -0700

    editing index, added route

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ from stock_scraper import get_data
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
 @app.route('/data')
 def data():
     return jsonify(get_data())
--- static/main.js
+++ static/main.js
@@ -1,1 +1,6 @@
-// custom javascript
+$(function () {
+
+    console.log('ready!');
+
+
+});
