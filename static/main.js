df621a802ef8e7af474a0cabfb28ccc882d7c40a
Josh Milo <josh@joshmilo.com>
2014-11-04 19:13:49 -0800

    adding more styles, mouse events.

--- static/main.js
+++ static/main.js
@@ -21,15 +21,24 @@ var createGraph = function() {
 
   d3.json("/data", function(error, quotes) {
     var node = svg.selectAll('.node')
-      .data(bubble.nodes(quotes).filter(function(d) { return !d.children; }));
+      .data(bubble.nodes(quotes)
+      .filter(function(d) { return !d.children; }))
       .enter().append('g')
       .attr('class', 'node')
       .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
 
       node.append('circle')
-        .attr('r', function(d) { return d.r; });
-        .style('fill', function(d) { return color(d.symbol); });
+        .attr('r', function(d) { return d.r; })
+        .style('fill', function(d) { return color(d.symbol); })
 
+        .on("mouseover", function(d) {
+          tooltip.text(d.name + ": $" + d.price);
+          tooltip.style("visibility", "visible");
+        })
+        .on("mousemove", function() {
+          return tooltip.style("top", (d3.event.pageY-10) + "px").style("left", (d3.event.pageX+10) + "px");
+        })
+        .on("mouseout", function() { return tooltip.style("visibility", "hidden";) });
       node.append('text')
         .attr('dy', '.3em')
         .style('text-anchor', 'middle')
@@ -46,7 +55,7 @@ var createGraph = function() {
     .style("background-color", "rgba(0, 0, 0, 0.75")
     .style("border-radius", "6px")
     .style("font", "12px sans-serif")
-    .text("tooltip")
+    .text("tooltip");
 
 
 } //end graph
